00786fbee49f6f8aa08aa85279a894f682ea46c6
Hunter Kingsbeer <63939038+hunterkingsbeer@users.noreply.github.com>
2021-04-19 14:47:26 +1200

    TESTER APP (NOT A PART OF OUR ALPHA APPLICATION)
    This is a small project I've made to allow group members to become familiar with parts of swift

---
+++ COSC345/COSC345/COSC345App.swift
@@ -0,0 +1,20 @@
+//
+//  COSC345App.swift
+//  COSC345
+//
+//  Created by Hunter Kingsbeer on 18/04/21.
+//
+
+import SwiftUI
+
+@main
+struct COSC345App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
