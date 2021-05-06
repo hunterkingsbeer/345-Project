4df90245fac0655c2793ba7610019104d200511b
hunterkingsbeer <kinhu152@student.otago.ac.nz>
2021-05-07 09:04:21 +1200

    Initial Commit

---
+++ COSC345-Project/COSC345_ProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  COSC345_ProjectApp.swift
+//  COSC345-Project
+//
+//  Created by Hunter Kingsbeer on 7/05/21.
+//
+
+import SwiftUI
+
+@main
+struct COSC345_ProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
