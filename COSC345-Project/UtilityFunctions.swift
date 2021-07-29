758bb51be1ebaad0e448197695097257e9793552
hunterkingsbeer <kinhu152@student.otago.ac.nz>
2021-07-29 21:33:48 +1200

    Unused code

--- COSC345-Project/UtilityFunctions.swift
+++ COSC345-Project/UtilityFunctions.swift
@@ -19,22 +19,6 @@ extension UIScreen {
    static let screenSize = UIScreen.main.bounds.size
 }
 
-extension View {
-    // FOUND AT https://www.avanderlee.com/swiftui/conditional-view-modifier/
-    /// Applies the given transform if the given condition evaluates to `true`.
-    /// - Parameters:
-    ///   - condition: The condition to evaluate.
-    ///   - transform: The transform to apply to the source `View`.
-    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-}
-
 extension UIDevice {
     var isSimulator: Bool {
         return TARGET_OS_SIMULATOR != 0
